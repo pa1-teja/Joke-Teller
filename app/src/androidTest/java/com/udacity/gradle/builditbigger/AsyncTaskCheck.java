508b36d172de5c4bda057f02b57e449fbc28c37c
pa1-teja <pavanteja.93@gmail.com>
2016-09-29 19:55:23 +0530

    Udacity Project 4 - Joke Telling app android test problem

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskCheck.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskCheck.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 
@@ -10,7 +11,20 @@ import android.util.Pair;
 import com.example.pavan.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.HttpResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -19,73 +33,74 @@ import java.util.concurrent.TimeUnit;
  */
 public class AsyncTaskCheck extends InstrumentationTestCase {
 
-    private static boolean called;
-    private Context context;
-    private static MyApi myApiService = null;
+    private final String LOG_TAG = AsyncTaskCheck.class.getSimpleName();
+    private CountDownLatch latch;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        called = false;
-    }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+    @Before
+    public void setUpTest(){
+        latch = new CountDownLatch(1);
 
-    public final void testSuccessfulFetch() throws Throwable {
-        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        fetchJokesAsyncTask fetchJokesAsyncTask= new fetchJokesAsyncTask() {
 
-        runTestOnUiThread(new Runnable() {
             @Override
-            public void run() {
-                context = getInstrumentation().getContext();
-                final EndpointsAsyncTask endpointsAsyncTask =  new EndpointsAsyncTask(){
-
-                    @Override
-                    protected String doInBackground(Pair<Context, String>... params) {
-                        if(myApiService == null) {  // Only do this once
-                            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                                    .setRootUrl("https://udacity-joke-tellar-app.appspot.com/_ah/api/");
-                            // end options for devappserver
-
-                            myApiService = builder.build();
-                        }
-
-                        context = params[0].first;
-                        String name = params[0].second;
-
-                        try {
-                            String responseString;
-                            responseString = myApiService.sayHi(name).execute().getData();
-
-                            return responseString;
-                        } catch (IOException e) {
-                            return e.getMessage();
-                        }
-                    }
-
-                    @Override
-                    protected void onPostExecute(String result) {
-
-                        fail("Hi, null");
-                        assertEquals("String is null",null,result);
-                        Log.i(getInstrumentation().getClass().getSimpleName(),"lauda lasson------" +result);
-                        countDownLatch.countDown();
-                    }
-                };
+            protected Object doInBackground(Object[] params) {
 
 
-                endpointsAsyncTask.execute(new Pair<Context, String>(getInstrumentation().getContext(),"null"));
+                HttpClient httpClient = new DefaultHttpClient();
+//        HttpPost httpPost = new HttpPost("http://udacity-joke-tellar-app.appspot.com/jokesApi");
+//        HttpGet httpGet  = new HttpGet("http://udacity-joke-tellar-app.appspot.com/jokesApi");
+//        HttpGet httpGet  = new HttpGet("http://192.168.1.2:8080/jokeApi");
+                HttpPost post = new HttpPost("http://192.168.1.2:8080/jokesApi");
+                String DEFAULT_RESPONSE = "No Joke Available";
+
+                org.apache.http.HttpResponse httpResponse = null;
 
                 try {
-                    countDownLatch.await(30,TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
+
+//            httpResponse = httpClient.execute(httpGet);
+                    httpResponse = httpClient.execute(post);
+                    // write response to log
+                    Log.d("Http Post Response:", httpResponse.toString());
+
+                    HttpEntity entity = httpResponse.getEntity();
+                    InputStream inputStream = entity.getContent();
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line = "";
+                    String result = "";
+                    while ((line = bufferedReader.readLine()) != null)
+                        result += line;
+
+                    inputStream.close();
+                    return result;
+
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
+                return DEFAULT_RESPONSE;
+
             }
-        });
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                assertNotNull(o);
+            }
+
+        };
+
+       fetchJokesAsyncTask.execute();
+
+        try {
+            latch.await(30,TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
