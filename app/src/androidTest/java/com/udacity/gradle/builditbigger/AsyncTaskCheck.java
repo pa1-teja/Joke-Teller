ff2c0ccf2a656c14c9f7fd20b2357aea8f0253b4
pa1-teja <pavanteja.93@gmail.com>
2016-09-29 21:58:34 +0530

    Udacity Project 4 - Joke Telling app android test problem

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskCheck.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskCheck.java
@@ -18,6 +18,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
